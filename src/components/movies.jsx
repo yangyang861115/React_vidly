e3fe94b3d07e9a2986364df02c5a36ee5b3203a3
Vivian Yang <vyang@silver-peak.com>
2018-10-07 16:59:21 -0700

    deploy

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -128,10 +128,6 @@ class Movies extends Component {
     } = this.state;
     const { user } = this.props;
 
-    if (count === 0) {
-      return <p>There are no movies in the database.</p>;
-    }
-
     const { totalCount, data: movies } = this.getPageData();
     return (
       <div className="row">
